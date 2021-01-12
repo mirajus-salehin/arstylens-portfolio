94a235b0571455a833eb0d34073c30027a86704f
mirajus-salehin <mirajussalehin@gmail.com>
2021-01-12 14:04:36 +0600

    map updated

--- map.js
+++ map.js
@@ -1,5 +1,11 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([25.6385571,88.634230], 16);
+
+
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+
+var marker = L.marker([25.6385571,88.634230]).addTo(mymap);
+
+marker.bindPopup("<b>Hey I'm here</b>").openPopup();
